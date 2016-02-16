c46bcdac13b1f463a23e5cad5aad540001f5ae30
Mike Brand <mbrand@appnexus.com>
2016-02-15 23:25:15 -0800

    I don’t remember

--- site/templates/my-work.php
+++ site/templates/my-work.php
@@ -4,7 +4,7 @@
 
 <div class = "row bottomBar">
 <div class = "columns small-12 large-12">
-<h1>My Work</h1>	
+<h1>&nbsp;</h1>	
 </div>
 </div><!--row-->	
 
@@ -13,8 +13,8 @@
 <div class = "row ixd">	
 	<?php $rowFull = 0; ?>
 	<?php foreach($page->children() AS $thisChild): ?>
-		<?php if($rowFull%3==0 && $rowFull!==0){echo "</div><!--row--><div class = 'row ixd'>";} ?>
-		<div class = "columns large-4 small-12 artBox">
+		<?php if($rowFull%4==0 && $rowFull!==0){echo "</div><!--row--><div class = 'row ixd'>";} ?>
+		<div class = "columns large-3 small-12 artBox">
 		
 			
 			<a href = "<?php echo $thisChild->url() ?>"><img src="<?php print_r($thisChild->images()->first()->thumb()->url()) ?>" alt="<?php print_r($thisChild->images()->first()->alt()) ?>" /></a>
